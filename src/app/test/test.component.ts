1b1826d265ffbb4fcbbffbe77167e40038d08d60
Mathieu <mathieuducharme86@gmail.com>
2024-02-19 22:49:01 -0500

    Merge

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -25,9 +25,6 @@ export class TestComponent {
             .pipe(
                 map( x => x.data.playcount )
             )
-            // .subscribe(playcountsData => {
-            //     console.log(playcountsData.data.playcount)
-            // })
     }
 }
 
